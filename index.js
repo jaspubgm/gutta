3d688ac3c8d8a825bd5cb104737f94b2a1a1e4b8
jaspubgm <jaspubgm@gmail.com>
2024-05-15 22:58:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//console.log('Hello')
+
+//window.alert('Hi this is Jazz website!')
+
+// This is a comment
+
+
+// h1 is defined as headings which is the head of the web
+//document.getElementById("myH1").textContent = 'Gutta';  
+
+// p is defined as paragrahp of a website
+//document.getElementById("myP").textContent = "Gutta gjengen i 10B er galt.";
+
